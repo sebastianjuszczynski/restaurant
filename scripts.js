3f31bf80506a61f1edfd9101c8f3abaf7cc4ab33
Sebastian Juszczynski <63791700+sebastianjuszczynski@users.noreply.github.com>
2026-03-15 12:17:39 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function greet() {
+    console.log("Welcome to the restaurant!");
+}
+
+function farewell() {
+    console.log("Thank you for visiting the restaurant!");
+}
+
+greet();
+farewell();
